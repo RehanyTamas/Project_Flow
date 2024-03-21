5da6b690f6cc058c9c267b9da72b5b3e0040c91f
RehanyTamas <Rt19991231>
2024-03-21 17:57:31 +0100

    create ProjectDetails element

---
+++ frontend/src/pages/ProjectDetails.jsx
@@ -0,0 +1,111 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import AppConfig from "../config";
+import {useParams} from "react-router-dom";
+
+const ProjectDetails = () => {
+
+    const { id } = useParams();
+    const [project, setProject] = useState(null);
+    const [token,setToken] = useState(null);
+
+    useEffect(() =>{
+        setToken( localStorage.getItem('loginToken'));
+
+    },[])
+
+    useEffect(() => {
+        if(token != null){
+            fetchProjectDetail(token,id)
+                .then((project) => {
+                    setProject(project);
+                });
+        }
+    }, [token]);
+
+    const fetchProjectDetail = async (token,id) => {
+
+        try {
+            const response = await axios.get(`${AppConfig.backendUrl}/api/my-projects/${id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching tasks:", error);
+            return [];
+        }
+    };
+
+    return (
+        <div key={project.id}>
+            <div>Project name: {project.name}</div>
+            <div>Project description: {project.description}</div>
+            <div>Project deadline: {project.deadline}</div>
+            <div>Project creator: {project.creator}</div>
+            <div>
+                <a> Add new</a>
+                <table>
+                    <thead>
+                    <tr>
+                        <th scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Team
+                            Member Name
+                        </th>
+                        <th scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Delete
+                        </th>
+                        <th/>
+                    </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                    {project.team_members.map((teamMember) => (
+                        <tr key={teamMember.id}>
+                            <td className="px-6 py-4 whitespace-nowrap">{teamMember.name}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">Delete</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <a> Add new</a>
+                <table>
+                    <thead>
+                    <tr>
+                        <th scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Task
+                            Name
+                        </th>
+                        <th scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description
+                        </th>
+                        <th scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Assigned To
+                        </th>
+                        <th scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status
+                        </th>
+                        <th/>
+                    </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                    {project.tasks.map((task) => (
+                        <tr key={task.id}>
+                            <td className="px-6 py-4 whitespace-nowrap">{task.name}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{task.description}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">{task.assigned_to}</td>
+                            <td className="px-6 py-4 whitespace-nowrap">Delete</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    )
+}
+
+export default ProjectDetails
