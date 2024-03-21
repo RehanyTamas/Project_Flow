740fabf12930feaff44da27d4ddefaade7303a8c
RehanyTamas <Rt19991231>
2024-03-21 16:41:11 +0100

    add get for users tasks in backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('my-tasks', [TaskController::class, 'getUserTasks']);
 });
 
 Route::post('register', [AuthController::class, 'register']);
