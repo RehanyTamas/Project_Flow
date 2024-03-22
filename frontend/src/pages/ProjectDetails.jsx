7c903683ca734b5e594c58a0f716de0b16c85337
RehanyTamas <Rt19991231>
2024-03-22 15:03:46 +0100

    refractor fronetnd for details page

--- frontend/src/pages/ProjectDetails.jsx
+++ frontend/src/pages/ProjectDetails.jsx
@@ -21,7 +21,7 @@ const ProjectDetails = () => {
                     setProject(project);
                 });
         }
-    }, [token]);
+    }, [token,id]);
 
     const fetchProjectDetail = async (token,id) => {
 
@@ -40,70 +40,71 @@ const ProjectDetails = () => {
     };
 
     return (
-        <div key={project.id}>
-            <div>Project name: {project.name}</div>
-            <div>Project description: {project.description}</div>
-            <div>Project deadline: {project.deadline}</div>
-            <div>Project creator: {project.creator}</div>
-            <div>
-                <a> Add new</a>
-                <table>
-                    <thead>
-                    <tr>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Team
-                            Member Name
-                        </th>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Delete
-                        </th>
-                        <th/>
-                    </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                    {project.team_members.map((teamMember) => (
-                        <tr key={teamMember.id}>
-                            <td className="px-6 py-4 whitespace-nowrap">{teamMember.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">Delete</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-            <div>
-                <a> Add new</a>
-                <table>
-                    <thead>
-                    <tr>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Task
-                            Name
-                        </th>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description
-                        </th>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Assigned To
-                        </th>
-                        <th scope="col"
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status
-                        </th>
-                        <th/>
-                    </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                    {project.tasks.map((task) => (
-                        <tr key={task.id}>
-                            <td className="px-6 py-4 whitespace-nowrap">{task.name}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{task.description}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{task.assigned_to}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">Delete</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
+        <div>
+            {project ? (
+                <div key={project.id}>
+                    <div>Project name: {project.name}</div>
+                    <div>Project description: {project.description}</div>
+                    <div>Project deadline: {project.deadline}</div>
+                    <div>Project creator: {project.creator}</div>
+                    <div>
+                        <table>
+                            <thead>
+                            <tr>
+                                <th scope="col"
+                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Team
+                                    Member Name
+                                </th>
+                                <th/>
+                            </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                            {project.team_members.map((teamMember) => (
+                                <tr key={teamMember.id}>
+                                    <td className="px-6 py-4 whitespace-nowrap">{teamMember.name}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div>
+                        <table>
+                            <thead>
+                            <tr>
+                                <th scope="col"
+                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Task
+                                    Name
+                                </th>
+                                <th scope="col"
+                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description
+                                </th>
+                                <th scope="col"
+                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Assigned
+                                    To
+                                </th>
+                                <th scope="col"
+                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status
+                                </th>
+                                <th/>
+                            </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                            {project.tasks.map((task) => (
+                                <tr key={task.id}>
+                                    <td className="px-6 py-4 whitespace-nowrap">{task.name}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{task.description}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{task.assigned_to}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{task.status}</td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            ) : <div> Loading</div>
+            }
         </div>
+
 
     )
 }
