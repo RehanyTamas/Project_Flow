2fe609e1709e3eb5d97655d81d2301d19ca25a74
RehanyTamas <Rt19991231>
2024-04-09 13:04:01 +0200

    add separation

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import {  Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import NavbarLoggedIn from "./NavbarLoggedIn";
+import Calendar from "react-calendar";
 
 const Layout = () => {
     const [isLoggedIn,setIsLoggedIn] = useState(false);
@@ -13,14 +14,36 @@ const Layout = () => {
 
     return (
         <div>
-            <div className="fixed top-0 w-full z-50">
+            <div className="fixed top-0 w-full z-50" id={"navbar"}>
                 {isLoggedIn ? <NavbarLoggedIn setIsLoggedIn={setIsLoggedIn} /> : <Navbar />}
             </div>
-            <section className="min-h-screen">
-                <Outlet context={[isLoggedIn, setIsLoggedIn]} />
-            </section>
+            <div >
+                {isLoggedIn ? (
+                    <div className="flex" id={"allContent"}>
+                        <div className="w-2/3 h-full" id={"dynamicContent"}>
+                            <div className="h-full">
+                                <Outlet context={[isLoggedIn, setIsLoggedIn]}/>
+                            </div>
+                        </div>
+                        <div className="w-1/3  flex flex-col" id={"staticContent"}>
+                            <div className="flex-1 mt-20" id={"newsFeed"}>Content B1</div>
+                            <div className="flex-1 bg-custom-dark border border-white" id={"Calendar"}>
+                                <Calendar
+                                    className={"w-full mx-auto bg-indigo-950 h-full text-lg"}
+                                ></Calendar>
+                            </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="min-h-screen">
+                        <Outlet context={[isLoggedIn, setIsLoggedIn]}/>
+                    </div>
+                )}
+            </div>
         </div>
     );
+
+
 }
 
 export default Layout
