ddf0afcce2cb6f7c3d276c8359de787e718d0385
RehanyTamas <Rt19991231>
2024-04-13 14:45:29 +0200

    create migration for notifications table

---
+++ backend/database/migrations/2024_04_13_115322_create_notifications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('action');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('task_id')->nullable();
+            $table->timestamp('timestamp')->default(now());
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade')->nullable();
+
+            $table->index('user_id');
+            $table->index('project_id');
+            $table->index('task_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
