bf8793fb4c4a63593c3abcbe12093d70aa474c84
RehanyTamas <Rt19991231>
2024-04-15 10:37:41 +0200

    slot in emailsending to noticiation handler

--- backend/app/Services/NotificationService.php
+++ backend/app/Services/NotificationService.php
@@ -9,21 +9,25 @@ class NotificationService
     public function personAddedToTeamMembers($userId, $projectId)
     {
         $this->createNotification($userId, 'teammember_assignment', $projectId);
+        EmailService::sendAddedToTeamEmail($userId,$projectId);
     }
 
     public function personRemovedFromTeamMembers($userId, $projectId)
     {
         $this->createNotification($userId, 'teammember_deassignment', $projectId);
+        EmailService::sendRemovedFromTeamEmail($userId,$projectId);
     }
 
     public function personAssignedToTask($userId, $taskId, $projectId)
     {
         $this->createNotification($userId, 'task_assignment', $projectId, $taskId);
+        EmailService::sendAssignedToTaskEmail($userId,$taskId,$projectId);
     }
 
     public function personDeassignedFromTask($userId, $taskId, $projectId)
     {
         $this->createNotification($userId, 'task_deassignment', $projectId, $taskId);
+        EmailService::sendDeassignedFromTaskEmail($userId,$taskId,$projectId);
     }
 
     public function taskStatusChanged($userId, $taskId, $projectId)
