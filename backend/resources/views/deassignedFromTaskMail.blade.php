5c6b4a6422d1876eb906cf030a45b5a27f036176
RehanyTamas <Rt19991231>
2024-04-15 10:32:27 +0200

    create blade views for emails

---
+++ backend/resources/views/deassignedFromTaskMail.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="UTF-8">
+<head>
+    <title>You have been deassigned from a task</title>
+</head>
+<body>
+<p>You have been deassigned from task {{$taskName}} in project {{$projectName}}</p>
+<p>Don't take it personally</p>
+</body>
+</html>
---
+++ backend/resources/views/removedFromTeamMail.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="UTF-8">
+<head>
+    <title>You have been removed from a team</title>
+</head>
+<body>
+<p>You have been removed from the team of {{$projectName}}</p>
+</body>
+</html>
