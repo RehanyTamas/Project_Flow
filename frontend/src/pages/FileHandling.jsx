3ff67776f9b242f78e41dceea02223b2dc1e6922
RehanyTamas <Rt19991231>
2024-05-14 13:21:30 +0200

    create files table

--- frontend/src/pages/FileHandling.jsx
+++ frontend/src/pages/FileHandling.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import AppConfig from "../config";
+import FilesTable from "../components/tables/FilesTable";
 
 const FileUpload = () => {
     const [files, setFiles] = useState([]);
@@ -56,7 +57,6 @@ const FileUpload = () => {
             });
             console.log('File uploaded successfully', response.data);
 
-            // Update files state with new file metadata
             getFilesMetadata();
         } catch (error) {
             console.error('Error uploading file', error);
@@ -66,20 +66,20 @@ const FileUpload = () => {
     const handleDownload = async (filename) => {
         try {
             const response = await axios.get(`${AppConfig.backendUrl}/api/my-files/${filename}`, {
-                responseType: 'blob', // Important for downloading files
+                responseType: 'blob',
                 headers: {
                     'Authorization': 'Bearer ' + token
                 }
             });
 
-            // Create a temporary anchor element to trigger download
+
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
             link.setAttribute('download', filename);
             document.body.appendChild(link);
             link.click();
-            // Cleanup
+
             window.URL.revokeObjectURL(url);
             link.parentNode.removeChild(link);
         } catch (error) {
@@ -104,34 +104,11 @@ const FileUpload = () => {
         <div className={"bg-custom-dark min-h-screen mt-20"}>
             <div className={"max-w-screen-md mx-auto shadow-md p-6 rounded-md"}>
                 <h2 className={"text-2xl font-semibold mb-4 text-white"}>Files</h2>
-                <table className={"min-w-full divide-y divide-gray-200"}>
-                    <thead className={"bg-custom-dark"}>
-                    <tr>
-                        <th className={"font-bold text-white border-r border-b border-white bg-indigo-950 px-6 py-3 text-center text-xs uppercase tracking-wider"}>Name</th>
-                        <th className={"font-bold text-white border-r border-b border-white bg-indigo-950 px-6 py-3 text-center text-xs uppercase tracking-wider"}>Size</th>
-                        <th className={"font-bold text-white border-r border-b border-white bg-indigo-950 px-6 py-3 text-center text-xs uppercase tracking-wider"}>Updated
-                            Date
-                        </th>
-                        <th className={"font-bold text-white border-r border-b border-white bg-indigo-950 px-6 py-3 text-center text-xs uppercase tracking-wider"}>Download</th>
-                        <th className={"font-bold text-white border-r border-b border-white bg-indigo-950 px-6 py-3 text-center text-xs uppercase tracking-wider"}>Delete</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {files.map((file, index) => (
-                        <tr key={index}>
-                            <td className={"px-6 py-4 whitespace-nowrap border-r ${index === projects.length - 1 ? 'border-b' : ''} border-white text-center text-white font-bold bg-blue-800"}>{file.name}</td>
-                            <td className={"px-6 py-4 whitespace-nowrap border-r ${index === projects.length - 1 ? 'border-b' : ''} border-white text-center text-white font-bold bg-blue-800"}>{file.size}</td>
-                            <td className={"px-6 py-4 whitespace-nowrap border-r ${index === projects.length - 1 ? 'border-b' : ''} border-white text-center text-white font-bold bg-blue-800"}>{file.updated_date}</td>
-                            <td className={"px-6 py-4 whitespace-nowrap border-r ${index === projects.length - 1 ? 'border-b' : ''} border-white text-center text-white font-bold bg-blue-800"}>
-                                <button className={"text-white hover:text-custom-dark"} onClick={() => handleDownload(`${file.name}.${file.extension}`)}>Download</button>
-                            </td>
-                            <td className={"px-6 py-4 whitespace-nowrap border-r ${index === projects.length - 1 ? 'border-b' : ''} border-white text-center text-white font-bold bg-blue-800"}>
-                                <button className={"text-white hover:text-custom-dark"} onClick={() => handleDelete(`${file.name}.${file.extension}`)}>Delete</button>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+                <FilesTable
+                    files={files}
+                    handleDownload={(fileName)=>handleDownload(fileName)}
+                    handleDelete={(filename) =>handleDelete(filename)}
+                />
                 <form className={"pt-8 text-white"} onSubmit={handleSubmit}>
                     <input className={"text-white font-bold"} type="file" onChange={handleFileChange}/>
                     <div className={"flex items-center justify-center"}>
