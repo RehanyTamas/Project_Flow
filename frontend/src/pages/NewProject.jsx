a79a43fb9521e29d37e173e9ec9f92768a92e591
RehanyTamas <Rt19991231>
2024-05-14 13:25:02 +0200

    insert projecttask and teammembers tables

--- frontend/src/pages/NewProject.jsx
+++ frontend/src/pages/NewProject.jsx
@@ -2,7 +2,9 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import AppConfig from "../config";
 import { useNavigate} from "react-router-dom";
-import TaskPopUp from "../components/TaskPopUp";
+import TaskPopUp from "../components/popups/TaskPopUp";
+import TeamMembersTable from "../components/tables/TeamMembersTable";
+import ProjectTaskTable from "../components/tables/ProjectTaskTable";
 
 const NewProject = () => {
 
@@ -143,80 +145,20 @@ const NewProject = () => {
                         required
                     />
                     <h2 className={"text-center font-bold text-white pb-2"}>Team members</h2>
-                    <table className="min-w-full divide-y divide-gray-200">
-                        <thead className="bg-indigo-950">
-                        <tr>
-                            <th scope="col"
-                                className="border-b border-r border-white px-6 py-3 text-center text-xs font-bold text-white bg-indigo-950 uppercase tracking-wider">Name
-                            </th>
-                            <th scope="col"
-                                className="border-b border-l border-white px-6 py-3 text-center text-xs font-bold text-white bg-indigo-950 uppercase tracking-wider">Add/Remove
-                            </th>
-                            <th/>
-                        </tr>
-                        </thead>
-                        <tbody className="bg-blue-900 divide-y divide-gray-200">
-                        {availablePeople.map((person) => (
-                            <tr key={person.id}>
-                                <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-r border-white px-6 py-4 whitespace-nowrap text-center">{person.name}</td>
-                                <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-l border-white px-6 py-4 whitespace-nowrap text-center">
-                                    {isPersonInTeam(person) ?
-                                        <button type="button" className={"hover:text-indigo-950"}
-                                                onClick={() => removePersonFromTeam(person)}>- Remove</button> :
-                                        <button type="button" className={"hover:text-indigo-950"} onClick={() => addPersonToTeam(person)}>+ Add</button>
-                                    }
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                    <TeamMembersTable
+                        availablePeople={availablePeople}
+                        isPersonInTeam={(person)=>isPersonInTeam(person)}
+                        removePersonFromTeam={(person)=>removePersonFromTeam(person)}
+                        addPersonToTeam={(person) => addPersonToTeam(person)}
+                    />
                 </div>
                 <h2 className={"text-center font-bold text-white pb-2"}>Tasks</h2>
-                <table className="min-w-full divide-y divide-gray-200 pb-2">
-                    <thead className="bg-indigo-950">
-                    <tr>
-                        <th scope="col"
-                            className="border-r border-b border-white px-6 py-3 text-center text-xs font-bold text-white uppercase tracking-wider">Name
-                        </th>
-                        <th scope="col"
-                            className="border-r border-b border-white px-6 py-3 text-center text-xs font-bold text-white uppercase tracking-wider">Description
-                        </th>
-                        <th scope="col"
-                            className="border-r border-b border-white px-6 py-3 text-center text-xs font-bold text-white uppercase tracking-wider">Deadline
-                        </th>
-                        <th scope="col"
-                            className="border-r border-b border-white px-6 py-3 text-center text-xs font-bold text-white uppercase tracking-wider">Assigned
-                            To
-                        </th>
-                        <th scope="col"
-                            className="border-r border-b border-white px-6 py-3 text-center text-xs font-bold text-white uppercase tracking-wider">Status
-                        </th>
-                        <th scope="col"
-                            className="border-l border-b border-white px-6 py-3 text-center text-xs font-bold text-white uppercase tracking-wider">Remove
-                        </th>
-                        <th/>
-                    </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                    {tasks.map((task, index) => (
-                        <tr key={index}>
-                            <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-r border-white px-6 py-4 whitespace-nowrap text-center bg-blue-900 text-white font-bold"
-                            >{task.name}</td>
-                            <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-r border-white px-6 py-4 whitespace-nowrap text-center bg-blue-900 text-white font-bold">
-                                {task.description}</td>
-                            <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-r border-white px-6 py-4 whitespace-nowrap text-center bg-blue-900 text-white font-bold">
-                                {task.deadline}</td>
-                            <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-r border-white px-6 py-4 whitespace-nowrap text-center bg-blue-900 text-white font-bold">
-                                {task.assignedTo}</td>
-                            <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-r border-white px-6 py-4 whitespace-nowrap text-center bg-blue-900 text-white font-bold">
-                                {task.status}</td>
-                            <td className="${index === availablePeople.length - 1 ? 'border-b' : ''} border-l border-white px-6 py-4 whitespace-nowrap text-center bg-blue-900 text-white font-bold">
-                                <button type="button" className={"hover:text-indigo-950"} onClick={() => removeTask(index)}>- Remove</button>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+                <ProjectTaskTable
+                    tasks={tasks}
+                    setTasks={(newTasks)=>setTasks(newTasks)}
+                    teamMembers={teamMembers}
+                    removeTask={(index)=>removeTask(index)}
+                />
                 <div className={"flex items-center justify-center"}>
                     <button
                         className="z-20 w-1/2 text-center py-3 rounded bg-transparent text-white font-bold focus:outline-none my-1 border border-transparent hover:border-white mb-1"
